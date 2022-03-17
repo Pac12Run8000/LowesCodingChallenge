d4c28b9caf76d43e0f4377c2c95b0ae22ec1d6a1
Pac12Run8000 <gangstarr78992@>
2022-03-17 07:05:26 -0700

    I added the searchbar and searchButton

--- MovieBrowser/Source/Detail/MovieDetailViewController.swift
+++ MovieBrowser/Source/Detail/MovieDetailViewController.swift
@@ -13,7 +13,6 @@ class MovieDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = UIColor.systemBlue
     }
 }
--- MovieBrowser/Source/Search/SearchViewController.swift
+++ MovieBrowser/Source/Search/SearchViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var searchButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = UIColor.systemBlue
+        self.title = "Movie Search"
     }
+    
+    @IBAction func searchButtonAction(_ sender: UIButton) {
+        print("Test")
+    }
+    
 }
